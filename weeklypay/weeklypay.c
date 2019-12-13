3e520119a7efc9548585a914d4be73a279bfe5fa
Amanda Kuenzle <amanda.kuenzle@buhlergroup.com>
2019-12-13 16:45:26 +0100

    commit for weeklypay challenge

---
+++ weeklypay/weeklypay.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    int userHours = 45;
+    float basicPayRate = 12.0;
+    int totalNormalHours;
+    float totalOverHours;
+    float grossPay;
+    float totalHours;
+    float afterTax1;
+    float afterTax2;
+    float afterTax3;
+    float netPay;
+
+    if (userHours > 40)
+    {
+        totalNormalHours = 40;
+        totalOverHours = 1.5 * (userHours - 40);
+    }
+    else
+    {
+        totalNormalHours = userHours;
+        totalOverHours = 0;
+    }
+
+    printf("Total normal hours: %d, Total over hours: %.1f\n", totalNormalHours, totalOverHours);
+
+    totalHours = totalNormalHours + totalOverHours;
+    printf("Total calculated hours: %.1f", totalHours);
+
+    grossPay = basicPayRate * totalHours;
+    printf("Total calculated gross pay: %.1f\n", grossPay);
+
+    if (grossPay > 450)
+    {
+        afterTax1 = (300.0 / 100.0) * 85.0;
+        afterTax2 = (150.0 / 100.0) * 80.0;
+        afterTax3 = ((grossPay - 450.0) / 100) * 75.0;
+    }
+    else
+    {
+        afterTax1 = (300.0 / 100.0) * 85.0;
+        afterTax2 = ((grossPay - 300.0) / 100.0) * 80.0;
+    }
+    
+     printf("afterTax1: %.1f\n", afterTax1);
+     printf("afterTax2: %.1f\n", afterTax2);
+     printf("afterTax3: %.1f\n", afterTax3);
+
+    netPay = afterTax1 + afterTax2 + afterTax3;
+    printf("Net pay: %.1f\n", netPay);
+
+    return 0;
+}
