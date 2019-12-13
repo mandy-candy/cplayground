a647395c9e42677adf6452b150e51a6e4f69933f
Amanda Kuenzle <amanda.kuenzle@buhlergroup.com>
2019-12-13 13:21:11 +0100

    finished some more lections

--- operators/operators.c
+++ operators/operators.c
@@ -4,4 +4,18 @@
 int main()
 {
     
+    double minutesInYear, years, days, userInput;
+    int minutesEntered = 0;
+
+    //get user input
+    printf("Enter minutes: ");
+    scanf("%d", &minutesEntered);
+
+    minutesInYear = (60 * 24 * 365);
+    years = (minutesEntered / minutesInYear);
+    days = (minutesEntered / 60.0) / 24.0;
+
+    printf("%d minutes is approximately %f years and %f days\n", minutesEntered, years, days);
+
+    return 0;
 }
